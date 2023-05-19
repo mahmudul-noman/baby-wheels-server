37f0a2579197a3e281ef9c22b15cfb80a7c5c35e
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-05-19 19:44:20 +0600

    added toy data show in All Toys Page UI

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
 
 
 
+        app.get('/allToys', async (req, res) => {
+            const result = await toysCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
