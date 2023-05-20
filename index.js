f3bc5ef06f81b1f86a5654acce59529a3e2fe7d5
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-05-20 18:56:36 +0600

    Implement Update Button

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uvmqfi7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,8 +24,6 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-
-
         const toysCollection = client.db('toyDB').collection('toys');
 
 
@@ -50,6 +47,36 @@ async function run() {
             const result = await toysCollection.find({ sellerEmail: req.params.email }).toArray();
             res.send(result);
         });
+
+        app.get('/allToys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toysCollection.findOne(query);
+            res.send(result);
+        })
+
+
+        app.put('/allToys/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            // const options = { upsert: true }
+            const updateToy = req.body;
+            const toy = {
+                $set: {
+                    toyPhoto: updateToy.toyPhoto,
+                    toyName: updateToy.toyName,
+                    sellerName: updateToy.sellerName,
+                    sellerEmail: updateToy.sellerEmail,
+                    toyPrice: updateToy.toyPrice,
+                    toyRating: updateToy.toyRating,
+                    quantity: updateToy.quantity,
+                    toyDetails: updateToy.toyDetails
+                }
+            }
+            const result = await toysCollection.updateOne(filter, toy)
+            res.send(result)
+        })
+
 
         app.delete('/allToys/:id', async (req, res) => {
             const id = req.params.id;
